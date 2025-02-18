8c48e1ba4985631c835630ecdc679ca4a783f223
Martin Statelov <statelov.martin@gmail.com>
2025-02-18 21:11:40 +0100

    Simple laser beam class

---
+++ Lasers/Assets/Scripts/LaserBeam.cs
@@ -0,0 +1,80 @@
+using System;
+using UnityEngine;
+
+[RequireComponent(typeof(LineRenderer))]
+public class LaserBeam : MonoBehaviour
+{
+    public Vector3 StartPosition;
+    public Vector3 EndPosition;
+    public Vector3 HitNormal;
+    //public Vector3 Direction => (EndPosition - StartPosition).normalized;
+
+    //public LaserBeam Prefab;
+
+    private const float _longestBeamDistance = 100f;
+
+    //private Mirror _mirrorTheBeamHit;
+    
+    private LineRenderer _lineRenderer;
+
+    //public Mirror MirrorTheBeamHit { 
+    //    get => _mirrorTheBeamHit; 
+    //    set {
+    //        if (_mirrorTheBeamHit == value) {
+    //            return;
+    //        }
+    //        else {
+    //            if (_mirrorTheBeamHit != null) {
+    //                _mirrorTheBeamHit.UnregisterLaserBeam(this);
+    //            }
+
+    //            _mirrorTheBeamHit = value;
+
+    //            if (_mirrorTheBeamHit != null) {
+    //                _mirrorTheBeamHit.RegisterLaserBeam(this);
+    //            }
+    //        }
+    //    }
+    //}
+
+
+    private void Awake() {                                                                                                               
+        _lineRenderer = GetComponent<LineRenderer>();
+        _lineRenderer.positionCount = 2;
+    }
+
+    public void Propagate(Vector3 startPosition, Vector3 direction) {
+        Vector3 endPosition = startPosition + direction * _longestBeamDistance;
+        Vector3 hitNormal = Vector3.zero;
+
+        if (Physics.Raycast(startPosition, direction, out RaycastHit hit, _longestBeamDistance)) {
+            endPosition = hit.point;
+            hitNormal = hit.normal;
+
+            //if (hit.collider.TryGetComponent(out Mirror mirror)) {
+            //    MirrorTheBeamHit = mirror;
+            //}
+            //else {
+            //    MirrorTheBeamHit = null;
+            //}
+        }
+        else {
+            //MirrorTheBeamHit = null;
+        }
+
+        StartPosition = startPosition;
+        EndPosition = endPosition;
+        HitNormal = hitNormal;
+        UpdateVisuals();
+
+        //if (MirrorTheBeamHit) {
+        //    MirrorTheBeamHit.Propagate(this);
+        //}
+    }
+
+    void UpdateVisuals() {
+        _lineRenderer.SetPosition(0, StartPosition);
+        _lineRenderer.SetPosition(1, EndPosition);
+    }
+
+}
---
+++ Lasers/Assets/Scripts/LaserSource.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class LaserSource : MonoBehaviour
+{
+    public Transform sourceTransform;
+    public LaserBeam laserBeam;
+
+
+    private void Update() {
+        Vector3 startPosition = sourceTransform.position;
+        Vector3 direction = sourceTransform.forward;
+
+        laserBeam.Propagate(startPosition, direction);
+    }
+
+}
